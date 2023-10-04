913c65b7ec8509fdefdddfc72a805dfae23b7655
PrafullBhise <praful.bhise@solventek.com>
2023-10-04 15:53:56 +0530

    updated

---
+++ src/users/ViewUser.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ViewUser() {
+  return (
+    <div className='conatiner'>
+         <div className='row'>
+                <div className='col-md-6 offset-md-3 border rounded p-4 mt-4 shadow bg-light'>
+                  <h2 className='text-center mt-2'>View Details</h2>
+                  </div>
+                  </div>
+                  </div>
+  )
+}
